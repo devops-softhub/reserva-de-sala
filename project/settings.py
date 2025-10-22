78f37b72fbdba0a1d983bafa16a31e4d63c60386
SBrayanAlves <sbrayanalves@gmail.com>
2025-10-22 19:10:03 -0300

    Estrutura do projeto - Reserva de Salas

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home.apps.HomeConfig",
+    "reservas.apps.ReservasConfig",
+    "salas.apps.SalasConfig",
+    "usuarios.apps.UsuariosConfig"
 ]
 
 MIDDLEWARE = [
